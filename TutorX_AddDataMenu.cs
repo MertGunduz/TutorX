48fe71f3b7408748bf80eee5a6d396658749c936
Mert Gunduz <merttgg@gmail.com>
2021-08-22 23:31:25 +0300

    TextBox Algorithm Improvement

--- TutorX_AddDataMenu.cs
+++ TutorX_AddDataMenu.cs
@@ -55,6 +55,11 @@ namespace TutorX
 
         private void AddStudent_Button_Click(object sender, EventArgs e)
         {
+            // Sets The TextBoxes To Old Texts
+            StudentName_TextBox.Text = "  Student Name:";
+            StudentSurname_TextBox.Text = "  Student Surname:";
+            StudentBirth_TextBox.Text = "  Student Birth:";
+
             oleDbConnection.Open();
             OleDbCommand addStudentCommand = new OleDbCommand("Insert Into Students_Table (Student_Name, Student_Surname, Student_Birth) Values (@p1, @p2, @p3)", oleDbConnection);
             addStudentCommand.Parameters.AddWithValue("@p1", StudentName_TextBox.Text);
@@ -66,6 +71,9 @@ namespace TutorX
 
         private void AddLesson_Button_Click(object sender, EventArgs e)
         {
+            // Sets The TextBoxes To Old Texts
+            LessonName_TextBox.Text = "  Lesson Name:";
+
             oleDbConnection.Open();
             OleDbCommand readLessonCommand = new OleDbCommand("Select Lesson_Name From Lessons_Table Where Lesson_Name = @p1", oleDbConnection);
             readLessonCommand.Parameters.AddWithValue("@p1", LessonName_TextBox.Text);
@@ -88,6 +96,12 @@ namespace TutorX
 
         private void AddTeacher_Button_Click(object sender, EventArgs e)
         {
+            // Sets The TextBoxes To Old Texts
+            TeacherName_TextBox.Text = "  Teacher Name:";
+            TeacherSurname_TextBox.Text = "  Teacher Surname:";
+            TeacherBirth_TextBox.Text = "  Teacher Birth:";
+            TeacherField_TextBox.Text = "  Teacher Field:";
+
             oleDbConnection.Open();
             OleDbCommand addTeacherCommand = new OleDbCommand("Insert Into Teachers_Table (Teacher_Name, Teacher_Surname, Teacher_Birth, Teacher_Field) Values (@p1, @p2, @p3, @p4)", oleDbConnection);
             addTeacherCommand.Parameters.AddWithValue("@p1", TeacherName_TextBox.Text);
