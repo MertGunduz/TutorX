b71b1412dec93d9636c9fb445a3f6724fc05803f
Mert Gunduz <merttgg@gmail.com>
2021-08-22 23:36:11 +0300

    UI Colors

--- TutorX_OperationSelectionMenu.Designer.cs
+++ TutorX_OperationSelectionMenu.Designer.cs
@@ -30,22 +30,123 @@ namespace TutorX
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TutorX_OperationSelectionMenu));
+            this.Mid_Panel = new System.Windows.Forms.Panel();
+            this.Bottom_Margin = new System.Windows.Forms.Panel();
+            this.Top_Margin = new System.Windows.Forms.Panel();
+            this.Minimize_Button = new System.Windows.Forms.Button();
+            this.Exit_Button = new System.Windows.Forms.Button();
+            this.Left_Margin = new System.Windows.Forms.Panel();
+            this.Right_Margin = new System.Windows.Forms.Panel();
+            this.Mid_Panel.SuspendLayout();
+            this.Top_Margin.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // Mid_Panel
+            // 
+            this.Mid_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(27)))), ((int)(((byte)(62)))));
+            this.Mid_Panel.Controls.Add(this.Bottom_Margin);
+            this.Mid_Panel.Controls.Add(this.Top_Margin);
+            this.Mid_Panel.Controls.Add(this.Left_Margin);
+            this.Mid_Panel.Controls.Add(this.Right_Margin);
+            this.Mid_Panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Mid_Panel.Location = new System.Drawing.Point(0, 0);
+            this.Mid_Panel.Name = "Mid_Panel";
+            this.Mid_Panel.Size = new System.Drawing.Size(800, 450);
+            this.Mid_Panel.TabIndex = 0;
+            // 
+            // Bottom_Margin
+            // 
+            this.Bottom_Margin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(27)))), ((int)(((byte)(62)))));
+            this.Bottom_Margin.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Bottom_Margin.Location = new System.Drawing.Point(10, 440);
+            this.Bottom_Margin.Name = "Bottom_Margin";
+            this.Bottom_Margin.Size = new System.Drawing.Size(780, 10);
+            this.Bottom_Margin.TabIndex = 12;
+            // 
+            // Top_Margin
+            // 
+            this.Top_Margin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(27)))), ((int)(((byte)(62)))));
+            this.Top_Margin.Controls.Add(this.Minimize_Button);
+            this.Top_Margin.Controls.Add(this.Exit_Button);
+            this.Top_Margin.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Top_Margin.Location = new System.Drawing.Point(10, 0);
+            this.Top_Margin.Name = "Top_Margin";
+            this.Top_Margin.Size = new System.Drawing.Size(780, 47);
+            this.Top_Margin.TabIndex = 11;
+            // 
+            // Minimize_Button
+            // 
+            this.Minimize_Button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Minimize_Button.FlatAppearance.BorderSize = 0;
+            this.Minimize_Button.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(27)))), ((int)(((byte)(62)))));
+            this.Minimize_Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(27)))), ((int)(((byte)(62)))));
+            this.Minimize_Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(27)))), ((int)(((byte)(62)))));
+            this.Minimize_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Minimize_Button.Image = global::TutorX.Properties.Resources.TutorX_MinimizeIcon;
+            this.Minimize_Button.Location = new System.Drawing.Point(722, 12);
+            this.Minimize_Button.Name = "Minimize_Button";
+            this.Minimize_Button.Size = new System.Drawing.Size(23, 23);
+            this.Minimize_Button.TabIndex = 1;
+            this.Minimize_Button.UseVisualStyleBackColor = true;
+            // 
+            // Exit_Button
+            // 
+            this.Exit_Button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Exit_Button.FlatAppearance.BorderSize = 0;
+            this.Exit_Button.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(27)))), ((int)(((byte)(62)))));
+            this.Exit_Button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(27)))), ((int)(((byte)(62)))));
+            this.Exit_Button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(27)))), ((int)(((byte)(62)))));
+            this.Exit_Button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Exit_Button.Image = global::TutorX.Properties.Resources.TutorX_ExitIcon;
+            this.Exit_Button.Location = new System.Drawing.Point(751, 12);
+            this.Exit_Button.Name = "Exit_Button";
+            this.Exit_Button.Size = new System.Drawing.Size(23, 23);
+            this.Exit_Button.TabIndex = 0;
+            this.Exit_Button.UseVisualStyleBackColor = true;
+            // 
+            // Left_Margin
+            // 
+            this.Left_Margin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(27)))), ((int)(((byte)(62)))));
+            this.Left_Margin.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Left_Margin.Location = new System.Drawing.Point(0, 0);
+            this.Left_Margin.Name = "Left_Margin";
+            this.Left_Margin.Size = new System.Drawing.Size(10, 450);
+            this.Left_Margin.TabIndex = 10;
+            // 
+            // Right_Margin
+            // 
+            this.Right_Margin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(27)))), ((int)(((byte)(62)))));
+            this.Right_Margin.Dock = System.Windows.Forms.DockStyle.Right;
+            this.Right_Margin.Location = new System.Drawing.Point(790, 0);
+            this.Right_Margin.Name = "Right_Margin";
+            this.Right_Margin.Size = new System.Drawing.Size(10, 450);
+            this.Right_Margin.TabIndex = 9;
             // 
             // TutorX_OperationSelectionMenu
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Mid_Panel);
             this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "TutorX_OperationSelectionMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TutorX";
+            this.Mid_Panel.ResumeLayout(false);
+            this.Top_Margin.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel Mid_Panel;
+        private System.Windows.Forms.Panel Bottom_Margin;
+        private System.Windows.Forms.Panel Top_Margin;
+        private System.Windows.Forms.Button Minimize_Button;
+        private System.Windows.Forms.Button Exit_Button;
+        private System.Windows.Forms.Panel Left_Margin;
+        private System.Windows.Forms.Panel Right_Margin;
     }
 }
